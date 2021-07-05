4c7cbcab576871fd0dc9d1ec92bdc3e2071de887
Alexander Hustler <alexhustler@users.noreply.github.com>
2021-07-05 16:30:59 +0000

    fix type error

--- pages/api/prices.ts
+++ pages/api/prices.ts
@@ -4,7 +4,7 @@ export default async function handler(req: any, res: any) {
     const productResponse = await commerce.getProduct({
       variables: { slug: req.query.slug },
       config: {},
-      preview: {},
+      preview: false,
     })
     const { price } = productResponse.product;
     res.json({ price })
