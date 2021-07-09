490ed376b1ad6d22ce6df3456b9ac3375bd19062
Alexander Hustler <alexhustler@users.noreply.github.com>
2021-07-09 00:55:10 +0000

    deploy

--- framework/swell/api/operations/get-all-products.ts
+++ framework/swell/api/operations/get-all-products.ts
@@ -25,6 +25,7 @@ export default function getAllProductsOperation({
     preview?: boolean
   } = {}): Promise<{ products: Product[] | any[] }> {
     const config = commerce.getConfig(cfg)
+    console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: ', variables.page)
     const { results } = await config.fetch('products', 'list', [
       {
         limit: variables.first,
